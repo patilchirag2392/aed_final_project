07658c23d54143b2084e848ccec23f9578390518
patilchirag2392 <patil.chirag@northeastern.edu>
2024-12-08 18:02:09 -0500

    push to main

---
+++ src/disastermanagement/DisasterManagement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package disastermanagement;
+
+/**
+ *
+ * @author chiragpatil
+ */
+public class DisasterManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
