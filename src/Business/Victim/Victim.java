a36087206bbef2802f806bef005227c82480a2b4
patilchirag2392 <patil.chirag@northeastern.edu>
2024-12-08 21:08:22 -0500

    updated UI and fixed bug

---
+++ src/Business/Victim/Victim.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Victim;
+
+import Business.WorkQueue.WorkQueue;
+
+/**
+ *
+ * @author Reema
+ */
+public class Victim {
+
+    private String name;
+    private String id;
+    private WorkQueue workqueue;
+
+    private static int count = 1;
+
+    //victim class constructor to autogenerate id
+    public Victim() {
+
+        id = "Victim" + (++count);
+    }
+
+    public WorkQueue getWorkqueue() {
+        return workqueue;
+    }
+
+    public void setWorkqueue(WorkQueue workqueue) {
+        this.workqueue = workqueue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/disaster/management/system_aed/pkgfinal/project/DisasterManagementSystem_AEDFinalProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package disaster.management.system_aed.pkgfinal.project;
+
+/**
+ *
+ * @author wadhwa
+ */
+public class DisasterManagementSystem_AEDFinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
